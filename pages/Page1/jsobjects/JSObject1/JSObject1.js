0b2f2993adcbe8a51ce4f617911d5a6fc0f7439e
Aparna Ramachandran <101863839+btsgh@users.noreply.github.com>
2026-01-20 12:20:25 +0530

    Update JSObject1.js

--- pages/Page1/jsobjects/JSObject1/JSObject1.js
+++ pages/Page1/jsobjects/JSObject1/JSObject1.js
@@ -2,7 +2,7 @@ export default {
 	myVar1: [],
 	myVar2: {},
 	myFun1 () {
-		console.log('This is a test message')
+		console.log('This is a test message to check for pull and commit')
 		//	write code here
 		//	this.myVar1 = [1,2,3]
 	},
